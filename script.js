af6993b9cc05cc294b346e7b87495f7fcc407dd9
smenesesd <meneses.s@northeastern.edu>
2026-09-21 16:40:55 -0400

    Script JS for buttom and CSS style also

---
+++ script.js
@@ -0,0 +1,4 @@
+function showInfo(){
+    document.getElementById("info").innerHTML =
+    "Campus Life Hub is a website designed to help students find useful information about campus events, sports, recreation, and other services.";
+}
