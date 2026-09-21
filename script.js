07639557449f902af2b01a54694eba4e7b63a406
smenesesd <meneses.s@northeastern.edu>
2026-09-21 16:46:25 -0400

    Script JS if/else function to show information and not

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function showInfo(){
-    document.getElementById("info").innerHTML =
-    "Campus Life Hub is a website designed to help students find useful information about campus events, sports, recreation, and other services.";
+    let info = document.getElementById("info");
+
+    if (info.innerHTML == "") {
+        info.innerHTML = "Campus Life Hub is a website designed to help students find useful information about campus events, sports, recreation, and other services.";
+    } else {
+        info.innerHTML = "";
+    }
+    
 }
